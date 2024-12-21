65aca23e70b705a465723476beb691c160f548ed
GaoMing <2949516603@qq.com>
2024-12-21 14:54:50 +0800

    11111

---
+++ y/src/components/hello.vue
@@ -0,0 +1,18 @@
+<div id="app">
+    <p>{{ reversedMessage }}</p>
+  </div>
+  
+  <script>
+    createApp({
+      data() {
+        return {
+          message: 'Hello'
+        };
+      },
+      computed: {
+        reversedMessage() {
+          return this.message.split('').reverse().join('');
+        }
+      }
+    }).mount('#app');
+  </script>
